8c1fd07b8d700f504dbfa6a054e0c601fc282a67
Mykola Papenko <46972880+mykelangelo@users.noreply.github.com>
2019-06-03 19:40:19 +0300

    Add spacing

--- src/server/router.js
+++ src/server/router.js
@@ -1,7 +1,9 @@
 module.exports =
     (apis, defaultAction) => ({
         detectAction: stringBeginningWithEndpoint => {
+            
             const foundApi = apis.find(api => stringBeginningWithEndpoint.startsWith(api.endpoint));
+            
             return foundApi ? foundApi.action : defaultAction;
         }
     });
